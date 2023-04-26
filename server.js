b984fb55412880fbf956c00b25073124492ed5c3
trupti0406 <truptivijayyadav27@gmail.com>
2023-04-26 15:23:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { MONGODB_URL } = require("./config");
+
+mongoose.connect(MONGODB_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log("Database Connected");
+});
+mongoose.connection.on("error", (error) => {
+  console.log("Some error occured while connecting to data");
+});
+app.use(cors());
+app.use(express.json());
+
+require("./models/user_model");
+require("./models/sales_model");
+
+// registering user schema
+app.use(require("./routes/user_route"));
+app.use(require("./routes/sales_route"));
+
+app.listen(PORT, () => {
+  console.log("Server started");
+});
